0555a6ae5e725481babe508edefdd51cf208e67c
Vijay Nailwal <vijay.naliwal@outlook.com>
2022-02-16 19:05:14 +0530

    asFlow

--- app/src/main/java/com/flow/examples/kotlinflow/MainActivity.kt
+++ app/src/main/java/com/flow/examples/kotlinflow/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.flow.examples.kotlinflow
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.flow.examples.util.LogUtil
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //convert list to flow
+        val data = listOf<Int>(1, 2, 3, 4).asFlow().flowOn(Dispatchers.IO)
+
+        runBlocking {
+            data.collect { it ->
+                LogUtil.d(it.toString())
+            }
+        }
     }
 }

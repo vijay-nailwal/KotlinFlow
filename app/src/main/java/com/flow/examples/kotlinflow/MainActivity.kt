a812b652f3a552a56044bec11a651772ce6b2058
Vijay Nailwal <vijay.naliwal@outlook.com>
2022-02-16 18:59:00 +0530

    flowOf()

--- app/src/main/java/com/flow/examples/kotlinflow/MainActivity.kt
+++ app/src/main/java/com/flow/examples/kotlinflow/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.flow.examples.kotlinflow
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.flow.examples.util.LogUtil
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val data = flowOf(1, 2, 3, 4, 6).onEach { delay(1000) }.flowOn(Dispatchers.IO)
+
+        runBlocking {
+            data.collect { it ->
+                LogUtil.d(it.toString())
+            }
+        }
     }
 }

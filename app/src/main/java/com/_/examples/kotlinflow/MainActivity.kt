c97650cdb1525d6087043c4484c1b5a5d844cd4d
Vijay Nailwal <vijay.naliwal@outlook.com>
2022-02-16 18:03:21 +0530

    no message

--- app/src/main/java/com/_/examples/kotlinflow/MainActivity.kt
+++ app/src/main/java/com/_/examples/kotlinflow/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.verizon.adop.kotlinflow
+package com._.examples.kotlinflow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- app/src/test/java/com/_/examples/kotlinflow/ExampleUnitTest.kt
+++ app/src/test/java/com/_/examples/kotlinflow/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.verizon.adop.kotlinflow
+package com._.examples.kotlinflow
 
 import org.junit.Test
 

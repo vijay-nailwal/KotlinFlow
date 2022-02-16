e2ccb2e5adc84e57b8893e8c6dbd6bad45204509
Vijay Nailwal <vijay.naliwal@outlook.com>
2022-02-16 19:13:11 +0530

    filter{ }

--- app/src/main/java/com/flow/examples/kotlinflow/MainActivity.kt
+++ app/src/main/java/com/flow/examples/kotlinflow/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.flow.examples.util.LogUtil
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
@@ -17,10 +14,10 @@ class MainActivity : AppCompatActivity() {
 
         val data = flowOf(1, 2, 3, 4, 5).flowOn(Dispatchers.IO)
 
-//        for changing the values use map
+//        for filter the values use filter
         runBlocking {
-            data.map { value ->
-                value * value
+            data.filter { value ->
+                value%2==0
             }.collect { it ->
                 LogUtil.d(it.toString())
             }

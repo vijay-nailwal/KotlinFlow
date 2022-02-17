8e367f6237a53ebeab1de74abd1ea49198dd9542
Vijay Nailwal <vijay.naliwal@outlook.com>
2022-02-17 14:14:34 +0530

    state flow alternative of live data

--- app/src/main/java/com/flow/examples/kotlinflow/viemodel/MainViewModel.kt
+++ app/src/main/java/com/flow/examples/kotlinflow/viemodel/MainViewModel.kt
@@ -7,10 +7,13 @@ package com.flow.examples.kotlinflow.viemodel
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 
-class MainViewModel() : ViewModel() {
+class MainViewModel : ViewModel() {
 
     private val _counterState: MutableStateFlow<Int>
     = MutableStateFlow(0)
+
+    //    val counterState:StateFlow<Int> = _counterState
+    //    or
     val counterState = _counterState
 
     fun incrementCount() {
